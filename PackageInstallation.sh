24ce8d7229d8ff60765b6a4edb8e10c57dd8edc1
Farshadc <96406162+farshad-c@users.noreply.github.com>
2021-12-26 00:03:20 +0530

    Add files via upload

---
+++ PackageInstallation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd \
+sudo apt-get install -y apt-transport-https bc dnsmasq hostapd vim python3-flask;
+sudo apt-get install -y python3-requests dirmngr accountsservice build-essential python quilt;
+sudo apt-get install -y devscripts python-setuptools python3 libssl-dev cmake libc-ares-dev;
+sudo apt-get install -y uuid-dev daemon zip zlibc zlib1g zlib1g-dev python3-smbus unclutter;
+sudo apt-get install -y matchbox-window-manager xwit xinit;
+sudo apt-get install -y lxterminal geoclue-2.0;
+sudo apt-get install -y --no-install-recommends xserver-xorg;
+sudo apt-get install -y --no-install-recommends x11-xserver-utils xinit openbox;
